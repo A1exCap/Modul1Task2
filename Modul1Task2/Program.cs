5498f1b8e7c7e20dbae7dc19e1d1fad99c58f5be
alex <alex1mulka@gmail.com>
2025-10-23 15:17:54 +0300

    Add project files.

---
+++ Modul1Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Modul1Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Array = new int[5,5];
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Array[i, j] = rand.Next(1, 101);
+                    Console.Write(Array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int[] rowsSum = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                   rowsSum[i] += Array[i, j];
+                }
+            }
+
+            Console.WriteLine();
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Sum of rows {i + 1}: {rowsSum[i]}");
+            }
+            Console.WriteLine();
+            Console.WriteLine(rowsSum.Max());
+        }
+    }
+}
